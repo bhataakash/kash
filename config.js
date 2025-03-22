82fc8e2551b2cfb18c17d6908457555d6d2939fa
MR AKASH <kashpappu00@gmail.com>
2025-03-22 15:24:50 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Configuration values for the application
-export const CONFIG = {
-  DAILY_BONUS_AMOUNT: 2, // Amount in rupees
-  INITIAL_WALLET_BALANCE: 5, // Starting balance
-  REFERRAL_BONUS: 3, // Referral bonus amount
-  MIN_WITHDRAWAL: 450, // Minimum withdrawal amount
-  TELEGRAM_BONUS: 5, // Telegram join bonus - updated from 1 to 5
-  TELEGRAM_LINK: "https://t.me/teamanonymous07"
-};
